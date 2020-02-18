5abc11e933999ffabff1aeb03e48ff51ba2b787e
Lexie Kemp <lexie.kd@gmail.com>
2020-02-17 16:35:58 -0800

    updated paths for dashboard

--- homeyess/website/dashboard/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('homeless/<?username>', views.homeless, name='homeless'),
-    path('company/<?username>', views.homeless, name='company'),
-    path('volunteer/<?username>', views.homeless, name='volunteer'),
-]
--- homeyess/website/views.py
+++ homeyess/website/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from website.forms import SignUpForm
+from .models import Profile, Ride, JobPost
 
 def index(request):
     return render(request, 'index.html')
@@ -25,3 +26,28 @@ def signup(request):
 
     return render(request, 'registration/signup.html', {'form': form})
 
+def dashboard(request, user_id):
+    user = Profile.objects.get(pk=user_id)
+    if user.user_type == "V":
+        return volunteer(request, user)
+    elif user.user_type == "H":
+        return homeless(request, user)
+    elif user.user_type == "C":
+        return company(request, user)
+
+def homeless(request, user):
+    unconfirmed_rides = Ride.objects.filter(homeless = user, ride_status = "U")
+    confirmed_rides = Ride.objects.filter(homeless = user, ride_status = "C")
+    context = {'user': user, 'unconfirmed_rides': unconfirmed_rides, 'confirmed_rides': confirmed_rides}
+    return render(request, 'dashboard/homeless.html', context)
+
+def company(request, user):
+    job_posts = JobPost.objects.filter(company=user)
+    context = {'user': user, 'job_posts': job_posts}
+    return render(request, 'dashboard/company.html', context)
+
+def volunteer(request, user):
+    confirmed_rides = Ride.objects.filter(volunteer = user, ride_status = "C")
+    finished_rides = Ride.objects.filter(volunteer = user, ride_status = "F")
+    context = {'user': user, 'confirmed_rides': confirmed_rides, 'finished_rides': finished_rides}
+    return render(request, 'dashboard/volunteer.html', context)
