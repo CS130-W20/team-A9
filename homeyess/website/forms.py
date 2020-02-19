73f2c0b04a5a36057efa71104c0aad665ead9d81
Quentin Truong <quentintruong@users.noreply.github.com>
2020-02-19 15:23:01 -0800

    Riderequest (#40)
    * Added some ride request functionality. Form still needs to be fixed
    
    * Fixed forms and now the request properly stores requests into a database. Need to still implement viewing requests and editing requests
    
    Co-authored-by: Hoang Nguyen <hnguyen247@gmail.com>

--- homeyess/website/forms.py
+++ homeyess/website/forms.py
@@ -4,6 +4,10 @@ homeyess/website/forms.py
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import ModelForm
+from django.db import models
+from .models import RideRequestPost
+from django.urls import reverse
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True)
@@ -14,3 +18,19 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'phone')
+
+class RideRequestForm(ModelForm):
+    class Meta:
+        model = RideRequestPost
+        fields = ['first_name', 'last_name', 'email', 'pickup_date', 'interview_duration', 'pickup_address', 'interview_address']
+
+    class DateInput(forms.DateInput):
+        input_type = 'date'
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    email = forms.EmailField(max_length=254, required=False, help_text='Optional')
+    pickup_date = forms.DateField(widget=DateInput, required=True)
+    interview_duration = forms.CharField(max_length=30, required=True)
+    pickup_address = forms.CharField(max_length=200, required=True)
+    interview_address = forms.CharField(max_length=200, required=True)
+    
--- homeyess/website/urls.py
+++ homeyess/website/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('accounts/signup', views.signup, name='signup'),
     path('dashboard/<str:user_id>/', views.dashboard, name='dashboard'),
+    path('request-ride', views.RequestRideCreate.as_view(), name='request_ride'),
 ]
