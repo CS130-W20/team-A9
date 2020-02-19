5bda5962dbbfaa8a134269af3691e610cb37d396
lexiekemp <lexie.kd@gmail.com>
2020-02-18 20:09:36 -0800

    Dashboard (#33)
    * dashboard templates
    
    * dashboard views and URL paths
    
    * updated models for dashboard details
    
    * for total_volunteer_minutes in model set null and blank to true
    
    * new migrations for dashboard details
    
    * updated paths for dashboard
    
    * make volunteer optional when creating a ride request
    
    * added created field for job posts
    
    * dashboard templates with css
    
    * filter rides using date and volunteer instead of ride status

--- homeyess/website/models.py
+++ homeyess/website/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils.timezone import now
 from enum import Enum
 
 
@@ -20,20 +21,30 @@ class Profile(models.Model):
 	car_plate = models.CharField(max_length=8, blank=True, null=True)
 	car_make = models.CharField(max_length=20, blank=True, null=True)
 	car_model = models.CharField(max_length=20, blank=True, null=True)
+	total_volunteer_minutes = models.IntegerField(blank=True, null=True)
 
 
 class Ride(models.Model):
+	class RideStatus(Enum):
+		Unconfirmed = 'U'
+		Confirmed = "C"
+		Finished = "F"
+		
 	homeless = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='ride_homeless_set')
-	volunteer = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, related_name='ride_volunteer_set')
+	volunteer = models.ForeignKey(Profile, on_delete=models.SET_NULL, blank=True, null=True, related_name='ride_volunteer_set')
 	interview_datetime = models.DateTimeField()
 	interview_duration = models.IntegerField()
 	volunteer_address = models.CharField(max_length=200, null=True)
 	pickup_address = models.CharField(max_length=200)
+	pickup_datetime = models.DateTimeField()
 	interview_address = models.CharField(max_length=200)
-
+	interview_company = models.CharField(max_length=100)
+	end_datetime = models.DateTimeField()
+	ride_status = models.CharField(max_length=100, choices=[(tag.value, tag.name) for tag in RideStatus], default=RideStatus.Unconfirmed)
 
 class JobPost(models.Model):
 	company = models.ForeignKey(Profile, on_delete=models.CASCADE)
+	created = models.DateField(auto_now_add=True)
 	last_edited = models.DateField(auto_now=True)
 	location = models.CharField(max_length=100)
 	wage = models.CharField(max_length=100)
@@ -41,7 +52,6 @@ class JobPost(models.Model):
 	job_title = models.CharField(max_length=100)
 	short_summary = models.CharField(max_length=200)
 	description = models.TextField()
-
 
 @receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
--- homeyess/website/urls.py
+++ homeyess/website/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('accounts/signup', views.signup, name='signup'),
+    path('dashboard/<str:user_id>/', views.dashboard, name='dashboard'),
 ]
