163d2cad84641eda30220255d16155c3108f107a
Dennis van Ee <dennis@vanee.us>
2020-02-19 16:35:43 -0800

    more changes

--- homeyess/website/forms.py
+++ homeyess/website/forms.py
@@ -4,14 +4,10 @@ homeyess/website/forms.py
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-<<<<<<< HEAD
 from django.forms import ModelForm
 from django.db import models
 from .models import RideRequestPost
 from django.urls import reverse
-=======
-from website.models import JobPost
->>>>>>> aee87a54ec4434ea8ef992c8562c52c40588857a
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True)
@@ -22,7 +18,6 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'phone')
-<<<<<<< HEAD
         
 class RideRequestForm(ModelForm):
     class Meta:
@@ -44,22 +39,11 @@ class JobForm(forms.ModelForm):
     location = forms.CharField(max_length=100, required=True)
     wage = forms.CharField(max_length=100, required=True)
     hours = forms.CharField(max_length=100, required=True)
-=======
-
-class JobForm(forms.ModelForm):
-    location = forms.CharField(max_length=100, required=True)
-    wage = forms.CharField(max_length=100, required=True) #forms.DecimalField(max_digits=6, decimal_places=2, min_value=0.0, required=True)
-    hours = forms.CharField(max_length=100, required=True) #forms.IntegerField(min_value=0, required=True)
->>>>>>> aee87a54ec4434ea8ef992c8562c52c40588857a
     job_title = forms.CharField(max_length=100, required=True)
     short_summary = forms.CharField(max_length=200, required=True)
     description = forms.CharField(widget=forms.Textarea, required=True)
 
     class Meta:
         model = JobPost
-<<<<<<< HEAD
         fields = ('location', 'wage', 'hours', 'job_title', 'short_summary', 'description')
 '''
-=======
-        fields = ('location', 'wage', 'hours', 'job_title', 'short_summary', 'description')
->>>>>>> aee87a54ec4434ea8ef992c8562c52c40588857a
--- homeyess/website/urls.py
+++ homeyess/website/urls.py
@@ -9,13 +9,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('accounts/signup', views.signup, name='signup'),
     path('dashboard/<str:user_id>/', views.dashboard, name='dashboard'),
-<<<<<<< HEAD
     path('request-ride', views.RequestRideCreate.as_view(), name='request_ride'),
     path('jobs/<str:pk>/post_job', views.PostJob.as_view(), name='postjob'),
     path('jobs/<str:user_id>/<int:pk>/edit_job', views.EditJob.as_view(), name='editjob'),
 ]
-=======
-    path('dashboard/<str:user_id>/', views.post_job, name='post_job'),
-    path('dashboard/<str:user_id>/<int:job_id>', views.edit_job, name='edit_job'),
-]
->>>>>>> aee87a54ec4434ea8ef992c8562c52c40588857a
