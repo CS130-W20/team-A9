c1a55a3d6533e53c2009511211d74afc7def5a83
Alex Hunsader <alex@hunsader.com>
2020-03-02 21:20:01 -0800

    Change signup to 2 pages, add view_ride for volunteer, change dashboard url (#79)
    * split signup forms
    
    * added margin to the bottom
    
    * added ride detail to volunteer dashboard
    
    * tell homeless that ride not confirmed yet
    
    * tested everything

--- homeyess/website/urls.py
+++ homeyess/website/urls.py
@@ -7,12 +7,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('accounts/signup', views.signup, name='signup'),
-    path('dashboard/<str:user_id>/', views.dashboard, name='dashboard'),
+    path('accounts/signup/<str:user_type>/', views.signup, name='signup'),
+    path('accounts/user-type', views.user_type, name='user_type'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('view-ride/<int:ride_id>/', views.viewRide, name='view_ride'),
     path('update-ride/<int:ride_id>/', views.RequestRideEdit.as_view(), name='update_ride'),
     path('request-ride', views.RequestRideCreate.as_view(), name='request_ride'),
-    path('delete-ride/<int:ride_id>/', views.DeleteRideRequest, name='delete_ride'),
+    path('delete-ride/<int:ride_id>/', views.deleteRideRequest, name='delete_ride'),
     path('jobs/', views.postjob, name='post_job'),
     path('jobs/<int:job_id>/', views.editjob, name='edit_job'),
     path('search_rides', views.ride_board, name='search_rides'),
