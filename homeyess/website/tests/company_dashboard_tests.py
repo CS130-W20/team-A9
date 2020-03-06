a35fa2ebd8d9f8a6070ba73a1ca9c6c8574e0341
quentintruong <quentintruong@users.noreply.github.com>
2020-03-06 00:28:32 -0800

    clean up spaces

--- homeyess/website/tests/company_dashboard_tests.py
+++ homeyess/website/tests/company_dashboard_tests.py
@@ -8,45 +8,45 @@ client = Client()
 
 class CompanyDashboardTest(TestCase):
 
-	def setUp(self):
-		#companies
-		self.c1_user = User.objects.create_user(username='testcompany1', password='hellopie')
-		self.c1_user.profile.user_type = "C"
-		self.c1_user.save()
+    def setUp(self):
+        #companies
+        self.c1_user = User.objects.create_user(username='testcompany1', password='hellopie')
+        self.c1_user.profile.user_type = "C"
+        self.c1_user.save()
 
-		self.c2_user = User.objects.create_user(username='testcompany2', password='hellopie')
-		self.c2_user.profile.user_type = "C"
-		self.c2_user.save()
+        self.c2_user = User.objects.create_user(username='testcompany2', password='hellopie')
+        self.c2_user.profile.user_type = "C"
+        self.c2_user.save()
 
-		#job posts 
-		self.job_1 = JobPost.objects.create(company=self.c1_user.profile, location="loc1", wage="2$/HR", hours="9-5", job_title="testjob1", short_summary="testing1", description="test test1")
+        #job posts 
+        self.job_1 = JobPost.objects.create(company=self.c1_user.profile, location="loc1", wage="2$/HR", hours="9-5", job_title="testjob1", short_summary="testing1", description="test test1")
 
-	def test_get_job_posts(self):
-		#test company 1 - has a job post
-		url = reverse("dashboard", kwargs={'user_id': self.c1_user.pk})
-		url = "/dashboard/" + str(self.c1_user.pk) + '/'
-		response = client.get(url)   
+    def test_get_job_posts(self):
+        #test company 1 - has a job post
+        url = reverse("dashboard", kwargs={'user_id': self.c1_user.pk})
+        url = "/dashboard/" + str(self.c1_user.pk) + '/'
+        response = client.get(url)   
 
-		self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-		#expected jobs posted by company c1_user
-		query_expected_response = JobPost.objects.filter(company=self.c1_user.profile)
-		expected_response = list(query_expected_response)
-		got_response = list(response.context['job_posts'])
-		self.assertEqual(got_response, expected_response)
+        #expected jobs posted by company c1_user
+        query_expected_response = JobPost.objects.filter(company=self.c1_user.profile)
+        expected_response = list(query_expected_response)
+        got_response = list(response.context['job_posts'])
+        self.assertEqual(got_response, expected_response)
 
-		#test company 2 - no job posts
-		url = reverse("dashboard", kwargs={'user_id': self.c2_user.pk})
-		url = "/dashboard/" + str(self.c2_user.pk) + '/'
-		response = client.get(url)   
+        #test company 2 - no job posts
+        url = reverse("dashboard", kwargs={'user_id': self.c2_user.pk})
+        url = "/dashboard/" + str(self.c2_user.pk) + '/'
+        response = client.get(url)   
 
-		self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-		#expected jobs posted by company c2_user
-		query_expected_response = JobPost.objects.filter(company=self.c2_user.profile)
-		expected_response = list(query_expected_response)
-		got_response = list(response.context['job_posts'])
-		self.assertFalse(got_response)
-		self.assertEqual(got_response, expected_response)
-		
+        #expected jobs posted by company c2_user
+        query_expected_response = JobPost.objects.filter(company=self.c2_user.profile)
+        expected_response = list(query_expected_response)
+        got_response = list(response.context['job_posts'])
+        self.assertFalse(got_response)
+        self.assertEqual(got_response, expected_response)
+        
 
--- homeyess/website/tests/message_tests.py
+++ homeyess/website/tests/message_tests.py
@@ -4,45 +4,45 @@ from website.ride_utils import send_message
 
 
 class MessageTestCase(TestCase):
-  def setUp(self):
-    self.valid = User.objects.create_user(username='valid', password='hellopie')
-    self.valid.profile.user_type = "V"
-    self.valid.profile.phone = "7148831948"
-    self.valid.profile.user.email = "email@email.com"
-    self.valid.save()
+    def setUp(self):
+        self.valid = User.objects.create_user(username='valid', password='hellopie')
+        self.valid.profile.user_type = "V"
+        self.valid.profile.phone = "7148831948"
+        self.valid.profile.user.email = "email@email.com"
+        self.valid.save()
 
-    self.no_phone = User.objects.create_user(username='no_phone', password='hellopie')
-    self.no_phone.profile.user_type = "V"
-    self.no_phone.profile.phone = ""
-    self.no_phone.profile.user.email = "email@email.com"
-    self.no_phone.save()
+        self.no_phone = User.objects.create_user(username='no_phone', password='hellopie')
+        self.no_phone.profile.user_type = "V"
+        self.no_phone.profile.phone = ""
+        self.no_phone.profile.user.email = "email@email.com"
+        self.no_phone.save()
 
-    self.no_email = User.objects.create_user(username='no_email', password='hellopie')
-    self.no_email.profile.user_type = "V"
-    self.no_email.profile.phone = "7148831948"
-    self.no_email.profile.user.email = ""
-    self.no_email.save()
+        self.no_email = User.objects.create_user(username='no_email', password='hellopie')
+        self.no_email.profile.user_type = "V"
+        self.no_email.profile.phone = "7148831948"
+        self.no_email.profile.user.email = ""
+        self.no_email.save()
 
-  def test_success(self):
-    res = send_message("test message", self.valid.profile)
-    
-    self.assertIsNotNone(res[0])
-    self.assertIsNotNone(res[1])
+    def test_success(self):
+        res = send_message("test message", self.valid.profile)
+        
+        self.assertIsNotNone(res[0])
+        self.assertIsNotNone(res[1])
 
-  def test_no_phone(self):
-    res = send_message("test message", self.no_phone.profile)
-    
-    self.assertIsNone(res[0])
-    self.assertIsNotNone(res[1])
+    def test_no_phone(self):
+        res = send_message("test message", self.no_phone.profile)
+        
+        self.assertIsNone(res[0])
+        self.assertIsNotNone(res[1])
 
-  def test_no_email(self):
-    res = send_message("test message", self.no_email.profile)
-    
-    self.assertIsNotNone(res[0])
-    self.assertIsNone(res[1])
+    def test_no_email(self):
+        res = send_message("test message", self.no_email.profile)
+        
+        self.assertIsNotNone(res[0])
+        self.assertIsNone(res[1])
 
-  def test_none(self):
-    res = send_message("test message", None)
-    
-    self.assertIsNone(res[0])
-    self.assertIsNone(res[1])
+    def test_none(self):
+        res = send_message("test message", None)
+        
+        self.assertIsNone(res[0])
+        self.assertIsNone(res[1])
