e52a964e4a5f3c7af834067774bc67768e68722d
quentintruong <quentintruong@users.noreply.github.com>
2020-03-07 15:37:48 -0800

    fix css on local dev

--- homeyess/settings.py
+++ homeyess/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "website/static/")
 STATIC_URL = "/static/"
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
